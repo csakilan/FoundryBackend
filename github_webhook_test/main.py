de1e9daaa67524fc65cfda8cbd944e987596cda6
Enaya Jawed <jawedenaya@gmail.com>
2025-11-01 19:14:46 -0500

    implement end-to-end CI/CD pipeline from GitHub push to EC2 deployment

--- github_webhook_test/main.py
+++ github_webhook_test/main.py
@@ -1,8 +1,16 @@
+import time
 from fastapi import FastAPI, Request
 import hmac
 import hashlib
 import os
 from dotenv import load_dotenv
+import requests
+from CICD.trigger_codebuild import trigger_codebuild
+from CICD.code_Deploy import codeDeploy
+from CICD.addYamlZip import addBuildSpec, addAppSpec,fastapi_buildspec_template, fastapi_appspec_template
+from CICD.deploymentScripts import addStartScript,start_sh_template,stop_sh_template,addStopScript,addInstallScript,install_sh_template
+from CICD.upload_s3 import upload_to_s3
+
 
 load_dotenv()
 
@@ -18,7 +26,6 @@ async def github_webhook(request: Request):
     """
     body = await request.body()
     signature_header = request.headers.get("X-Hub-Signature-256", "")
-
     secret = os.getenv("GITHUB_WEBHOOK_SECRET", "").encode()
 
     # verify signature if secret exists
@@ -26,10 +33,42 @@ async def github_webhook(request: Request):
         digest = hmac.new(secret, body, hashlib.sha256).hexdigest()
         expected = f"sha256={digest}"
         if not hmac.compare_digest(expected, signature_header):
+            print("Invalid webhook signature")
             return {"message": "Invalid signature"}
 
     payload = await request.json()
     print("Webhook received!")
-    print(payload)
 
-    return {"message": "Webhook received successfully!"}
+    repo_url = payload["repository"]["clone_url"]
+
+    owner = repo_url.split("/")[3]
+    repo = repo_url.split("/")[4].replace(".git", "")
+    ref = payload["ref"].split("/")[-1]
+
+    zip_url = f"https://api.github.com/repos/{owner}/{repo}/zipball/{ref}"
+    out_file = f"{repo}-{ref}.zip" 
+    S3_BUCKET_NAME = "foundry-codebuild-zip"
+    S3_KEY = f"{owner}/{out_file}"
+
+    response = requests.get(zip_url, allow_redirects=True)
+    if response.status_code == 200:
+        with open(out_file, "wb") as f:
+            f.write(response.content)
+        path = addBuildSpec(out_file, fastapi_buildspec_template, overWrite=True)
+        addAppSpec(out_file, fastapi_appspec_template, overWrite=True)
+        addStopScript(out_file, stop_sh_template, overWrite=True)
+        addInstallScript(out_file, install_sh_template, overWrite=True)
+        addStartScript(out_file, start_sh_template, overWrite=True)
+    else:
+        print(f"Failed to download repo: {response.status_code}")
+        return {"message": "Download failed"}
+
+    upload_to_s3(out_file, S3_BUCKET_NAME, S3_KEY)
+    time.sleep(10)
+
+    build_status = trigger_codebuild("foundryCICD", S3_BUCKET_NAME, S3_KEY, path, f"{owner}-{repo}")
+    if build_status["build_status"] == "SUCCEEDED":
+        codeDeploy(owner, repo, "foundry-artifacts-bucket", f"founryCICD-{owner}-{repo}")
+        return {"message": "Build and deploy completed successfully"}
+    else:
+        return {"message": "Build failed, skipping deploy"}
