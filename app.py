50702b96ce07b6487fc7fa28c2cb0eb25e146a71
Enaya Jawed <jawedenaya@gmail.com>
2025-11-03 10:45:40 -0600

    fix and re-tested code, ready for merge again

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import canvas
+from routers.github_webhook import router as github_router
 
 
 
@@ -19,6 +20,4 @@ app.add_middleware(
 )
 
 app.include_router(canvas.router)
-
-
-
+app.include_router(github_router)
